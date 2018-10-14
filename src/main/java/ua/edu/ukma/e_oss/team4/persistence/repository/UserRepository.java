01ca2447365d188686028d1a0e38031e96da2eee
njilrem <antonkucher00@gmail.com>
2018-10-14 03:08:45 +0300

    service

---
+++ src/main/java/ua/edu/ukma/e_oss/team4/persistence/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package ua.edu.ukma.e_oss.team4.persistence.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ua.edu.ukma.e_oss.team4.entity.User;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    List<User> findAll();
+    
+}
